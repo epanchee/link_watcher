d50c935f99a641f69b3540f5b400969c9b97632f
Timofey <epanchee@gmail.com>
2021-02-22 15:47:59 +0500

    Added(various fetchitem type support): raw and class affiliation

--- fetcher/agents.py
+++ fetcher/agents.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta, abstractmethod
 from urllib import request
 
 import lxml.html as lh
@@ -15,26 +16,30 @@ class FetchAgent:
         for fetch_item in self.fetch_items:
             fetched_group = []
             for item in fetch_item:
-                fetched_group.extend(item.seek(tree))
+                fetched_group.extend(item.seek_n_procces(tree))
             yield fetch_item.name, fetched_group
 
 
-class FetchItem:
+class BaseFetchItem(metaclass=ABCMeta):
 
     def __init__(
             self,
             related: 'List[FetchItem] or FetchItem' = None,
             xpath: str = '',
             primary: bool = False,
-            name: str = ''
+            name: str = '',
+            item_type: str = 'content',
+            params: dict = None,
     ):
         self.name = name
         self.xpath = xpath
         self.related = related if related else []
         self.primary = primary
+        self.item_type = item_type
+        self.params = params
 
-    def seek(self, tree):
-        return tree.xpath(self.xpath)[0].itertext()
+    def seek_n_procces(self, tree):
+        return self.process(self.seek(tree))
 
     def __iter__(self):
         yield self
@@ -42,4 +47,40 @@ class FetchItem:
             yield item
 
     def __str__(self):
-        return f'name: {self.name}, xpath: {self.xpath}, related: {self.related}, primary: {self.primary}'
+        return f'name: {self.name}, item_type: {self.item_type},' \
+               f' xpath: {self.xpath}, related: {self.related}, primary: {self.primary}'
+
+    def process(self, data):
+        return data
+
+    @abstractmethod
+    def seek(self, tree):
+        pass
+
+
+class FetchItem(BaseFetchItem):
+    """
+    Simple FetchItem to retrieve raw data from DOM-element without processing
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def seek(self, tree):
+        return tree.xpath(self.xpath)[0].itertext()
+
+
+class ClassFetchItem(BaseFetchItem):
+    """
+    FetchItem to check whether DOM-element has class from given set self.params['classes'] or not
+    """
+
+    def seek(self, tree):
+        return tree.xpath(self.xpath)[0].classes
+
+    def check_inside(self, data):
+        params = self.params.get('classes', set())
+        return data in self.params['classes'] if isinstance(params, set) else False
+
+    def process(self, data):
+        return self.name if self.check_inside(data) else ''
